40c5be6ba04c74b4d391bdd1677879e128a42dab
Julien Bataille <julien.bataille@mail.rakuten.com>
2014-09-09 13:06:24 +0900

    Initial commit

---
+++ WindowsStoreQrCode/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using ZXing;
+
+namespace WindowsStoreQrCode
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var barcodeWriter = new BarcodeWriter
+            {
+                Renderer = new ZXing.Rendering.PixelDataRenderer(),
+                Format = BarcodeFormat.QR_CODE,
+                Options = { Height = 250, Width = 250, Margin = 1 }
+            };
+
+            var qrPixelData = barcodeWriter.Write("http://zxingnet.codeplex.com/");
+
+            // ToBitmap() returns an object...
+            var qrCodeBitmap = qrPixelData.ToBitmap();
+            // but it's actually a WriteableBitmap 
+            MyQRCode.Source = qrCodeBitmap as WriteableBitmap;
+        }       
+    }
+}
